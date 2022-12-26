21c59ee3355f75ed73b7f0bc5a6bba9f02bb8b72
Samy Cheklat <samycheklat@hotmail.fr>
2022-12-25 19:44:00 -0500

    ${commitMessage}

--- website/deploy.py
+++ website/deploy.py
@@ -6,8 +6,8 @@ p1 = subprocess.run("git add .", shell=True)
 
 p2 = subprocess.run("git commit -m ${commitMessage}", shell=True)
 
-p3 = subprocess.run("git push")
+p3 = subprocess.run("git push", shell=True)
 
-p4 = subprocess.run("npm run build")
+p4 = subprocess.run("npm run build", shell=True)
 
-p5 = subprocess.run("firebase deploy")
+p5 = subprocess.run("firebase deploy", shell=True)
