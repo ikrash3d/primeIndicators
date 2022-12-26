226e658ed022e9380cc91b8b56dab14b9591359a
Samy Cheklat <samycheklat@hotmail.fr>
2022-12-25 19:45:18 -0500

    ${commitMessage}

--- website/deploy.py
+++ website/deploy.py
@@ -2,12 +2,12 @@ import subprocess
 
 commitMessage = input("Please enter a commit message with the double quotation mark: ")
 
-p1 = subprocess.run("git add .", shell=True)
+subprocess.run("git add .", shell=True)
 
-p2 = subprocess.run("git commit -m ${commitMessage}", shell=True)
+subprocess.run("git commit -m ${commitMessage}", shell=True)
 
-p3 = subprocess.run("git push", shell=True)
+subprocess.run("git push", shell=True)
 
-p4 = subprocess.run("npm run build", shell=True)
+subprocess.run("npm run build", shell=True)
 
-p5 = subprocess.run("firebase deploy", shell=True)
+subprocess.run("firebase deploy", shell=True)
--- website/src/components/footer/Footer.js
+++ website/src/components/footer/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className={styles.container}>
       <p>
-        <a></a> Prime Indicator 2022
+        <a>©</a> Prime Indicator 2022
       </p>
     </div>
   );
