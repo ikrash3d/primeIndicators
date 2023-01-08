14d0b2791ac2a6123ae35834fa11b802ce0d3ff2
Samy Cheklat <samycheklat@hotmail.fr>
2023-01-08 18:57:01 -0500

    {commitMessage}

--- website/deploy.py
+++ website/deploy.py
@@ -2,9 +2,11 @@ import subprocess
 
 commitMessage = input("Please enter a commit message with the double quotation mark: ")
 
+print("This is the commit message {commitMessage}")
+
 subprocess.run("git add .", shell=True)
 
-subprocess.run("git commit -m ${commitMessage}", shell=True)
+subprocess.run("git commit -m {commitMessage}", shell=True)
 
 subprocess.run("git push", shell=True)
 
